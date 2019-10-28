9021bc17d42674373d0c22a58aa85c912631604f
Saufi Rahman <saufi@fairtech.com.sg>
2019-10-28 22:06:07 +0800

    adding dotenv

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token_bot = 'NjM4MzY2MDQ3Mjg3NzcxMTU2.Xbbssw.5LDtOGTqg8MUiYxj2PebQeBfJmo';
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on('message', msg => {
-    if (msg.content === '!ping') {
+    if (msg.content === 'ping') {
         msg.reply('Pong!')
     }
 });
 
-
-client.login(token_bot);
+client.login(process.env.BOT_TOKEN);
